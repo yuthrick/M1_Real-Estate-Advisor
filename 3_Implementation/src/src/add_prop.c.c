aa9458ab50093d7134ae81d2cff0dce635eb5ae8
yuthrick <62828197+yuthrick@users.noreply.github.com>
2022-02-10 15:48:37 +0530

    Update add_prop.c.c

--- 3_Implementation/src/src/add_prop.c.c
+++ 3_Implementation/src/src/add_prop.c.c
@@ -1,25 +1,3 @@
-/**
- * @file funs.h
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2021-04-15
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-
-/**
- * @file funs.h
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2021-04-15
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-
 #include"funs.h"
 //#include"stdio.h"
 
