7cf76f65bd430d8dc6a8cc151ec8759bfcefd40c
yuthrick <62828197+yuthrick@users.noreply.github.com>
2022-02-10 15:55:03 +0530

    Update display_prop.c.c

--- 3_Implementation/src/src/display_prop.c.c
+++ 3_Implementation/src/src/display_prop.c.c
@@ -1,33 +1,3 @@
-/******************************************************************************
-
-                            Online C Compiler.
-                Code, Compile, Run and Debug C program online.
-Write your code in this editor and press "Run" button to compile and execute it.
-
-*******************************************************************************/
-
-/**
- * @file funs.h
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2021-04-15
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-
-/**
- * @file funs.h
- * @author your name (you@domain.com)
- * @brief 
- * @version 0.1
- * @date 2021-04-15
- * 
- * @copyright Copyright (c) 2021
- * 
- */
-
 #include"funs.h"
 
 void display_prop()
